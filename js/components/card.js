2b6450cfb959362652eb6f8e7a3dd8f14c9d2f00
JessicaTatham <jectatham@gmail.com>
2019-01-08 20:22:40 -0800

    Trello card board

---
+++ js/components/card.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Card = React.createClass({
+	render: function() {
+		return (
+			<p> This is a card </p>
+		)
+	}
+});
+
+module.exports = Card;
---
+++ js/components/list.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Card = require('./card');
+
+var List = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		)
+	}
+});
+
+module.exports = List;
